78aebc6abac35270c10e73e47f18483008ea1220
Aleksandra <abaibik@gmail.com>
2023-08-30 15:07:25 +0200

    add data.js

---
+++ data.js
@@ -0,0 +1,44 @@
+[
+  {
+    id: 1,
+    question: "Which designer invented the polo shirt?",
+    answer: "Rene Lacoste",
+  },
+
+  {
+    id: 2,
+    question: "What was Coco Chanel's real first name?",
+    answer: "Gabrielle",
+  },
+
+  {
+    id: 3,
+    question: "Which Bronx born fashion designer was named Ralph Lifshitz?",
+    answer: "Ralf Lauren",
+  },
+
+  {
+    id: 4,
+    question:
+      "Who once wore an outfit made out of raw beef to the MTV music awards?",
+    answer: "Lady Gaga",
+  },
+
+  {
+    id: 5,
+    question: "Who wore a Robocop outfit to the 2007 BET Awards?",
+    answer: "Beyoncé",
+  },
+
+  {
+    id: 6,
+    question: "Cashmere comes from which animal's fur?",
+    answer: "Goat",
+  },
+
+  {
+    id: 7,
+    question: "What would you be wearing if you were in your birthday suit?",
+    answer: "Nothing",
+  },
+];
