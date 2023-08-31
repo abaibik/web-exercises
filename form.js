8275b345a045e4aacd49e1c1fdefe1e7af7afabc
Aleksandra <abaibik@gmail.com>
2023-08-31 15:32:12 +0200

    fix css

--- form.js
+++ form.js
@@ -14,22 +14,23 @@ form.addEventListener("submit", (event) => {
 
   const newCard = document.createElement("article");
   newCard.classList.add("card");
+  newCard.classList.add("small-card");
   formContainer.append(newCard);
 
   newCard.innerHTML = `
-        <a id="bookmarkButton" class="bookmark-button"
+        <a id="bookmarkButton" class="bookmark-button small-card-bookmark"
             ><span id="bookmarkIcon" class="material-symbols-outlined icon">
             bookmark
             </span>
         </a>
         <div>
-            <p id="question">${data.question}</p>
-            <p id="answer" class="hidden">${data.answer}</p>
+            <p id="question">Question: ${data.question}</p>
+            <p id="answer" >Answer: ${data.answer}</p>
         </div>
-
-        <a id="answerButton" class="answer-button" role="button">Show Answer</a>
-
         <div class="tags">
-            <a class="tag">${data.tag}</a>
+            <a class="tag">#${data.tag}</a>
         </div>`;
+
+  document.body.classList.add("body-fresh");
+  formContainer.classList.add("container-fresh");
 });
